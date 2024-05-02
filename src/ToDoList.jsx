30cbf9fad2946a53a16ea4e9dcdff1d72e6e61f1
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-05-01 22:18:24 -0400

    Starting React Project: To Do List

---
+++ src/ToDoList.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function ToDoList() {
+  const [todos, setTodos] = useState([]);
+  const [newtodo, setNewTodo] = useState("");
+
+  return <></>;
+}
+
+export default ToDoList;
