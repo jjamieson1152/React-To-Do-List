072301c708e341a7fb01808fa984142aa19d31d5
Jimmy Jamieson <jjamieson1152@gmail.com>
2024-05-08 22:39:58 -0400

    Move Todo Up & Move Todo Down

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,37 +1,49 @@
 import { useState } from "react";
 
 function ToDoList() {
-  const [todos, setTodos] = useState(["Feed the Dog", "Take Out Trash", "Practice Coding"]);
+  const [todos, setTodos] = useState([
+    "Feed the Dog",
+    "Take Out Trash",
+    "Practice Coding",
+  ]);
   const [newTodo, setNewTodo] = useState("");
 
   function handleInputChange(event) {
-    setNewTodo(event.target.value)
+    setNewTodo(event.target.value);
   }
 
   function addToDo() {
-
     if (newTodo.trim() !== "") {
-      setTodos(t => [...t, newTodo]);
+      setTodos((t) => [...t, newTodo]);
       setNewTodo("");
     }
   }
 
   function deleteToDo(index) {
-    
     const updatedTodos = todos.filter((_, i) => i !== index);
-    setTodos(updatedTodos); 
+    setTodos(updatedTodos);
   }
 
   function moveToDoUp(index) {
-
+    if (index > 0) {
+      const updatedTodos = [...todos];
+      [updatedTodos[index], updatedTodos[index - 1]] = 
+      [updatedTodos[index - 1], updatedTodos[index]];
+      setTodos(updatedTodos);
+    }
   }
 
   function moveToDoDown(index) {
-
+    if (index < todos.length) {
+      const updatedTodos = [...todos];
+      [updatedTodos[index], updatedTodos[index + 1]] = 
+      [updatedTodos[index + 1], updatedTodos[index]];
+      setTodos(updatedTodos);
+    }
   }
 
   return (
-    <div className="to-do-list"> 
+    <div className="to-do-list">
       <h1>To Do List</h1>
 
       <div>
@@ -41,42 +53,29 @@ function ToDoList() {
           value={newTodo}
           onChange={handleInputChange}
         />
-        <button
-          className="add-button"
-          onClick={addToDo}
-        >
+        <button className="add-button" onClick={addToDo}>
           Add
         </button>
       </div>
 
       <ol>
-        {todos.map((todo, index) => 
+        {todos.map((todo, index) => (
           <li key={index}>
             <span className="text">{todo}</span>
-            <button
-              className="delete-button"
-              onClick={() => deleteToDo(index)}
-            >
+            <button className="delete-button" onClick={() => deleteToDo(index)}>
               Delete
             </button>
-            <button
-              className="move-button"
-              onClick={() => moveToDoUp(index)}
-            >
+            <button className="move-button" onClick={() => moveToDoUp(index)}>
               👆
             </button>
-            <button
-              className="move-button"
-              onClick={() => moveToDoDown(index)}
-            >
+            <button className="move-button" onClick={() => moveToDoDown(index)}>
               👇
             </button>
           </li>
-        )}
+        ))}
       </ol>
-
     </div>
-  )
+  );
 }
 
 export default ToDoList;
